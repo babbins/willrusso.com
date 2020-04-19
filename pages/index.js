6045da265a46d3566660fffbb71d1f6945c43035
Ali Mahmoud <ali.mahmoud@harrys.com>
2020-04-18 20:41:11 -0400

    init next.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Will Russo</h1>
+      <h3>A friend and a scholar.</h3>
+      <img src="https://picsum.photos/200/300" />
+    </div>
+  );
+}
