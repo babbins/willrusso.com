f1512c0aeb958e88145deb6536dbef30cdbd0ebe
Ali Mahmoud <ali.mahmoud@harrys.com>
2020-04-18 21:00:40 -0400

    add image

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ export default function Index() {
     <div>
       <h1>Will Russo</h1>
       <h3>A friend and a scholar.</h3>
-      <img src="https://picsum.photos/200/300" />
+      <img src="https://ibb.co/C2Y7z8j" />
+      <p>Website coming soon.</p>
     </div>
   );
 }
